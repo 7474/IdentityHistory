93d5d281cd5466970db078f17e0fdb7d62eefdb0
koudenpa <koudenpa@hotmail.com>
2020-03-22 23:22:00 +0900

    ライブラリプロジェクトをローカルで参照しながら認証統合を試している。

--- IdentityHistoryViewer/Program.cs
+++ IdentityHistoryViewer/Program.cs
@@ -5,20 +5,38 @@ using System.Text;
 using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using IdentityHistoryViewer.Service;
+using BlazorWebAsmEasyAuth;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace IdentityHistoryViewer
 {
     public class Program
     {
 #if DEBUG
+        const string SITE_BASE_URI = "https://localhost:5001";
         const string API_BASE_URI = "http://localhost:7071/";
 #else
+        const string SITE_BASE_URI = "https://identityhistory.z11.web.core.windows.net/";
         const string API_BASE_URI = "https://identityhistoryfunctionapp.azurewebsites.net/";
 #endif
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddSingleton(new EasyAuthConfig()
+            {
+                BlazorWebsiteURL = SITE_BASE_URI,
+                AzureFunctionAuthURL = API_BASE_URI,
+            });
+            builder.Services.AddScoped<EasyAuthNavigationHelper>();
+            builder.Services.AddScoped<EasyAuthAuthenticationStateProvider>();
+            builder.Services.AddScoped<AuthenticationStateProvider, EasyAuthAuthenticationStateProvider>();
+            // https://github.com/dotnet/aspnetcore/issues/18733
+            // Call AddAuthorizationCore after register AuthenticationStateProvider.
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+
             builder.Services.AddSingleton(new IHAPIConfig()
             {
                 BaseUri = API_BASE_URI,
